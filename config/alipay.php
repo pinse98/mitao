7ff50d8b24a3a8766f8b7bb0eeeccc524e701c40
shuaijinchao <shuaijinchao@octinn.com>
2015-11-09 17:23:32 +0800

    更正公钥

--- config/alipay.php
+++ config/alipay.php
@@ -44,7 +44,8 @@ return [
 	| 签名方式 不需修改
 	|
 	*/
-    'sign_type' => strtoupper('RSA'),
+    // 'sign_type' => strtoupper('RSA'),
+    'sign_type' => strtoupper('MD5'),
 
     /*
 	|--------------------------------------------------------------------------
